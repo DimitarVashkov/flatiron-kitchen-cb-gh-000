2126c14e02a3bd70e728ab560db6f93ef9796b0f
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-12 17:51:41 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -15,9 +15,14 @@ class IngredientsController < ApplicationController
   end
 
   def create
+    @ingredient = Ingredient.new
   end
 
   def update
     @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ing_params
+    params.require(:ingredient).permit(:name)
   end
 end
