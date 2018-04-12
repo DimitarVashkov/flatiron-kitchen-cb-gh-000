1e397fb4557b0182847c18384fa6da2b357b32cf
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-12 18:03:34 +0000

    Done.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,11 +3,36 @@ class IngredientsController < ApplicationController
   end
 
   def show
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def edit
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ing_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render :new
+    end
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(ing_params)
+      redirect_to @ingredient
+    else
+      render :edit
+    end
+  end
+
+  def ing_params
+    params.require(:ingredient).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 FlatironKitchen::Application.routes.draw do
-  get 'ingredients/index'
-
-  get 'ingredients/show'
-
-  get 'ingredients/edit'
-
-  get 'ingredients/new'
-
-  get 'recipes/index'
-
-  get 'recipes/show'
-
-  get 'recipes/edit'
-
-  get 'recipes/new'
+  resources :recipes
+  resources :ingredients
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,7 +42,7 @@ FlatironKitchen::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
